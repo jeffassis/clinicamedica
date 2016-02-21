566011ee6d0c3d8c9b366a68a98c898321dadb8c
jeff- <jeff-@192.168.1.5>
2016-02-21 06:50:52 -0300

    Configuração do Método main chamando a tela principal

--- src/aplicacao/Launch.java
+++ src/aplicacao/Launch.java
@@ -1,9 +1,39 @@
 package aplicacao;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author jeff-
  */
-public class Launch {
-    
+public class Launch extends Application {
+
+    /**
+     * Sobrecarga do método start para ser chamado o parametro Stage, utilizando
+     * o Parent com o FXMLoader para carregar o FXML
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Home.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Tela Principal");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * Método main para fazer a Chamada do argumento
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
