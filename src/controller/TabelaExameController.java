c0c2fca1df14455aeaa3fe320c4898f97a9fda5a
jeff- <jeff-@192.168.1.5>
2016-03-07 11:00:43 -0300

    Duvida TabelaExameController

---
+++ src/controller/TabelaExameController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author jeff-
+ */
+public class TabelaExameController implements Initializable {
+
+    private HomeController controller;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void onSave() {
+        /* TODO aqui e a dificuldade pois eu queria chamar o metodo la da Class
+        HomeController o Método e o precoExame
+        */
+    }
+}
