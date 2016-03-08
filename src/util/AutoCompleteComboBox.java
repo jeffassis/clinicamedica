4f9f47d837366ed755cd4d2d5b5b81336f4decd4
jeand <jeand@DESKTOP-2RTE1M2>
2016-03-07 22:37:00 -0300

    Bug consertado

--- src/util/AutoCompleteComboBox.java
+++ src/util/AutoCompleteComboBox.java
@@ -22,6 +22,8 @@ public class AutoCompleteComboBox implements EventHandler<KeyEvent> {
     private ComboBox comboBox;
     /*Lista que vai conter os dados originais do comboBox*/
     private ObservableList dados;
+    /*Variavel que vai dizer se o método saveData foi executado*/
+    private boolean salvouDados;
 
     public AutoCompleteComboBox(ComboBox comboBox) {
         /*Pego a referencia*/
@@ -29,6 +31,8 @@ public class AutoCompleteComboBox implements EventHandler<KeyEvent> {
         /*Altero o evento para ele ouvir esta classe uso o KeyReleased
         pq o evento é chamado só apos a palavra está digitada*/
         this.comboBox.setOnKeyReleased(this);
+        /*Informamos que o comboBox é editavel para o usuario digitar o nome*/
+        this.comboBox.setEditable(true);
 
     }
 
@@ -39,8 +43,8 @@ public class AutoCompleteComboBox implements EventHandler<KeyEvent> {
     public void saveData() {
         /*Salvamos os dados originais*/
         dados = comboBox.getItems();
-        /*Informamos que o comboBox é editavel para o usuario digitar o nome*/
-        this.comboBox.setEditable(true);
+        this.salvouDados = true;
+        
     }
 
     /**
@@ -50,8 +54,8 @@ public class AutoCompleteComboBox implements EventHandler<KeyEvent> {
      */
     @Override
     public void handle(KeyEvent event) {
-        /*So acontecera se o comboBox for editado.*/
-        if (comboBox.isEditable()) {
+        /*So acontecera se o metodo saveData foi executado*/
+        if (salvouDados) {
             /*Se foi apertado o botão UP(pra cima)*/
             if (event.getCode() == KeyCode.UP) {
                 /*Move o digitador para a ultima opção*/
