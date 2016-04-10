3dd66308c285728a411299bbccdb74fb07c8b8dc
jeand <jeand@DESKTOP-2RTE1M2>
2016-04-10 14:16:36 -0300

    Implementado Classe pai com os metodos utilizados

---
+++ src/util/Funcionalidades.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author jeanderson
+ */
+public class Funcionalidades {
+
+    /**
+     * Método para carregamento de Tabelas(TableView).
+     */
+    public void carregarTabela() {
+        //Deve ser implementada por quem utilizar esta classe.
+    }
+
+    /**
+     * Método que altera os dados dos componentes para dados iniciais.
+     */
+    public void refresh() {
+        //Deve ser implementada por quem utilizar esta classe.
+    }
+
+    /**
+     * Processos iniciais sem parâmetros.
+     */
+    public void iniciarProcessos() {
+        //Deve ser implementada por quem utilizar esta classe.
+    }
+
+    /**
+     * Processos iniciais com parâmetros.
+     *
+     * @param editar
+     * @param object
+     */
+    public void iniciarProcessos(boolean editar, Object object) {
+        //Deve ser implementada por quem utilizar esta classe.
+    }
+
+    /**
+     * Processos Iniciais com parêmetro que recebe uma Referencia.
+     * @param referencia 
+     */
+    public void iniciarProcessos(Object referencia) {
+        //Deve ser implementada por quem utilizar esta classe.
+    }
+}
