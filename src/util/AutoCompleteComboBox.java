d9da7395e5171a77f096691c77f1a98e8ed544bd
Jeanderson Silva Lopes <jeananimax@gmail.com>
2016-04-25 15:07:17 -0300

    Feito melhorias no javaDoc e no addComboBox

--- src/util/AutoCompleteComboBox.java
+++ src/util/AutoCompleteComboBox.java
@@ -18,111 +18,141 @@ import javafx.scene.input.KeyEvent;
  */
 public class AutoCompleteComboBox implements EventHandler<KeyEvent> {
 
-    /*Sera a referencia do Combo passado no construtor*/
-    private ComboBox comboBox;
-    /*Lista que vai conter os dados originais do comboBox*/
-    private ObservableList dados;
-    /*Variavel que vai dizer se o método saveData foi executado*/
-    private boolean salvouDados;
+	/* Sera a referencia do Combo passado no construtor */
+	private ComboBox comboBox;
+	/* Lista que vai conter os dados originais do comboBox */
+	private ObservableList dados;
+	/* Variavel que vai dizer se o método saveData foi executado */
+	private boolean salvouDados;
 
-    public AutoCompleteComboBox(ComboBox comboBox) {
-        /*Pego a referencia*/
-        this.comboBox = comboBox;
-        /*Altero o evento para ele ouvir esta classe uso o KeyReleased
-        pq o evento é chamado só apos a palavra está digitada*/
-        this.comboBox.setOnKeyReleased(this);
-        /*Informamos que o comboBox é editavel para o usuario digitar o nome*/
-        this.comboBox.setEditable(true);
+	public AutoCompleteComboBox(ComboBox comboBox) {
+		/* Pego a referencia */
+		this.comboBox = comboBox;
+		/*
+		 * Altero o evento para ele ouvir esta classe uso o KeyReleased pq o
+		 * evento é chamado só apos a palavra está digitada
+		 */
+		this.comboBox.setOnKeyReleased(this);
+		/* Informamos que o comboBox é editavel para o usuario digitar o nome */
+		this.comboBox.setEditable(true);
 
-    }
+	}
 
-    /**
-     * Método que salvas os dados do comboBox, devera ser usado apos os dados no
-     * comboBox serem populados.
-     */
-    public void saveData() {
-        /*Salvamos os dados originais*/
-        dados = comboBox.getItems();
-        this.salvouDados = true;
+	/**
+	 * Método que salvas os dados do comboBox, devera ser usado apos os dados no
+	 * comboBox serem populados.
+	 */
+	public void saveData() {
+		/* Salvamos os dados originais */
+		dados = comboBox.getItems();
+		this.salvouDados = true;
 
-    }
+	}
 
-    /**
-     * Evento é chamado toda vez que é alterado algum caractere no ComboBox.
-     *
-     * @param event
-     */
-    @Override
-    public void handle(KeyEvent event) {
-        /*So acontecera se o metodo saveData foi executado*/
-        if (salvouDados) {
-            /*Se foi apertado o botão UP(pra cima)*/
-            if (event.getCode() == KeyCode.UP) {
-                /*Move o digitador para a ultima opção*/
-                moveCaret(comboBox.getEditor().getText().length());
-                /*Para aqui*/
-                return;
-            } else if (event.getCode() == KeyCode.DOWN) {
-                /*Se o comboBox não estiver mostrando a lista*/
-                if (!comboBox.isShowing()) {
-                    comboBox.show();
-                }
-                moveCaret(comboBox.getEditor().getText().length());
-                /*Para aqui*/
-                return;
-            }
-            if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.LEFT
-                    || event.isControlDown() || event.getCode() == KeyCode.HOME
-                    || event.getCode() == KeyCode.END || event.getCode() == KeyCode.TAB) {
-                /*Se for utilizado qualquer uma dessas Teclas paramos o código aqui*/
-                return;
-            }
-            /*Essa lista vai conter os dados que vai ser exibido conforma o usuario digitar*/
-            ObservableList lista = FXCollections.observableArrayList();
-            for (int i = 0; i < dados.size(); i++) {
-                /*Como não sabemos qual o tipo do combobox, usamos o getConverter para retornar a StringConverter que foi adicionado nele
-                por meio da Classe ConverterDados, tendo essa String podemos comparar as Inicias dos nome, com o nome digitado no comboBox
-                utilizamos o metodo toLowerCase que transforma todas as letras em maiusculas.*/
-                if (comboBox.getConverter().toString(dados.get(i)).toLowerCase().startsWith(comboBox.getEditor().getText().trim().toLowerCase())) {
-                    /*os Nome que tiveram as mesmas inicias serao adicionadas a lista*/
-                    lista.add(dados.get(i));
-                }
-            }
-            /*Salva o texto digitado no comboBox pq vamos utilizar o setItens ai o texto seria perdido*/
-            String textoDigitado = comboBox.getEditor().getText();
+	/**
+	 * Evento é chamado toda vez que é alterado algum caractere no ComboBox.
+	 *
+	 * @param event
+	 */
+	@Override
+	public void handle(KeyEvent event) {
+		/* So acontecera se o metodo saveData foi executado */
+		if (salvouDados) {
+			/* Se foi apertado o botão UP(pra cima) */
+			if (event.getCode() == KeyCode.UP) {
+				/* Move o digitador para a ultima opção */
+				moveCaret(comboBox.getEditor().getText().length());
+				/* Para aqui */
+				return;
+			} else if (event.getCode() == KeyCode.DOWN) {
+				/* Se o comboBox não estiver mostrando a lista */
+				if (!comboBox.isShowing()) {
+					comboBox.show();
+				}
+				moveCaret(comboBox.getEditor().getText().length());
+				/* Para aqui */
+				return;
+			}
+			if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.LEFT || event.isControlDown()
+					|| event.getCode() == KeyCode.HOME || event.getCode() == KeyCode.END
+					|| event.getCode() == KeyCode.TAB) {
+				/*
+				 * Se for utilizado qualquer uma dessas Teclas paramos o código
+				 * aqui
+				 */
+				return;
+			}
+			/*
+			 * Essa lista vai conter os dados que vai ser exibido conforma o
+			 * usuario digitar
+			 */
+			ObservableList lista = FXCollections.observableArrayList();
+			for (int i = 0; i < dados.size(); i++) {
+				/*
+				 * Como não sabemos qual o tipo do combobox, usamos o
+				 * getConverter para retornar a StringConverter que foi
+				 * adicionado nele por meio da Classe ConverterDados, tendo essa
+				 * String podemos comparar as Inicias dos nome, com o nome
+				 * digitado no comboBox utilizamos o metodo toLowerCase que
+				 * transforma todas as letras em maiusculas.
+				 */
+				if (comboBox.getConverter().toString(dados.get(i)).toLowerCase()
+						.startsWith(comboBox.getEditor().getText().trim().toLowerCase())) {
+					/*
+					 * os Nome que tiveram as mesmas inicias serao adicionadas a
+					 * lista
+					 */
+					lista.add(dados.get(i));
+				}
+			}
+			/*
+			 * Salva o texto digitado no comboBox pq vamos utilizar o setItens
+			 * ai o texto seria perdido
+			 */
+			String textoDigitado = comboBox.getEditor().getText();
 
-            /*Alteramos para os itens dessa nova lista, assim ficando somente os que foram encontrados na condição acima (if)*/
-            comboBox.setItems(lista);
-            /*Colocamos o texto que tinha sido digitado*/
-            comboBox.getEditor().setText(textoDigitado);
-            /*Movemos o digitador para a ultima posição*/
-            moveCaret(textoDigitado.length());
-            /*Se a lista não estiver vazia*/
-            if (!lista.isEmpty()) {
-                comboBox.show();
-            }
+			/*
+			 * Alteramos para os itens dessa nova lista, assim ficando somente
+			 * os que foram encontrados na condição acima (if)
+			 */
+			comboBox.setItems(lista);
+			/* Colocamos o texto que tinha sido digitado */
+			comboBox.getEditor().setText(textoDigitado);
+			/* Movemos o digitador para a ultima posição */
+			moveCaret(textoDigitado.length());
+			/* Se a lista não estiver vazia */
+			if (!lista.isEmpty()) {
+				comboBox.show();
+			}
 
-        }
-    }
+		}else{
+			System.err.println("ERRO: AutoCompleteComboBox\n"
+					+ "Método saveData não foi chamado!\nÉ obrigatorio a chamada do método saveData");
+		}
+	}
 
-    /**
-     * Move o digitador para determinada posicação.
-     *
-     * @param length
-     */
-    private void moveCaret(int length) {
-        this.comboBox.getEditor().positionCaret(length);
-    }
+	/**
+	 * Move o digitador para determinada posicação.
+	 *
+	 * @param length
+	 */
+	private void moveCaret(int length) {
+		this.comboBox.getEditor().positionCaret(length);
+	}
 
-    /**
-     * Adiciona outro comboBox. Obs: Utilizar somente apos o comboBox está com
-     * todos os Dados populados.
-     *
-     * @param comboBox
-     */
-    public void addComboBox(ComboBox comboBox) {
-        AutoCompleteComboBox box = new AutoCompleteComboBox(comboBox);
-        box.saveData();
-    }
+	/**
+	 * Adiciona outro comboBox. Obs: Utilizar somente apos o comboBox está com
+	 * todos os Dados populados.
+	 * @param comboBox
+	 * @throws - Caso o comboBox esteja vázio.
+	 */
+	public void addComboBox(ComboBox comboBox) throws NullPointerException {
+		if (comboBox.getItems().size() > 0) {
+			AutoCompleteComboBox box = new AutoCompleteComboBox(comboBox);
+			box.saveData();
+		} else {
+			throw new NullPointerException();
+		}
+	}
 
 }
